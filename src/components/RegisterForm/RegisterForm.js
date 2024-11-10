93224c1e1ce490e2ed0060e4993123fd485bc670
dsalvatierra <dss250583@gmail.com>
2024-11-10 23:52:42 +0100

    FIX Completed RegisterForm

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -7,52 +7,50 @@ import { PadelMatches } from "../../pages/PadelMatches/PadelMatches.js";
 export const RegisterForm = (form) => {
     form.className = "register-form";
     form.innerHTML = `
-    <h2>Registro de Usuario</h2>
+    <h2>Registro de Usuarios</h2>
     ${FieldForm({ labelText: "Nombre de Usuario:", inputType: "text", inputPlaceholder: "Nombre de Usuario" })}
     ${FieldForm({ labelText: "Email:", inputType: "email", inputPlaceholder: "email@email.email" })}
     ${FieldForm({ labelText: "Contraseña:", inputType: "password", inputPlaceholder: "********" })}
     ${FieldForm({ labelText: "Teléfono:", inputType: "tel", inputPlaceholder: "123456789" })}
     ${FieldForm({ labelText: "Imagen de perfil:", inputType: "file" })}
-    <button type="submit">Crear Cuenta</button>
     `;
+    // <button type="submit">Crear Cuenta</button>
+    // form.addEventListener("submit", console.log());
 
-    form.addEventListener("submit", registerUser());
-
-    // form.append(
-    //     Button({
-    //         text: "Crear Cuenta",
-    //         fnc: async () => {},
-    //         className: "btn-registerForm"
-    //     })
-    // );
+    form.append(
+        Button({
+            text: "Crear Cuenta",
+            fnc: async () => {registerUser()},
+            className: "btn-registerForm"
+        })
+    );
 };
 
-export const registerUser = async (e) => {
-    const URL = "http://localhost:3000/api/v1/appadel/users";
+const registerUser = async (e) => {
     e.preventDefault();
-    const formData = new FormData(e);
+    const formData = new FormData();
     formData.append("name", e.target[0].value);
     formData.append("email", e.target[1].value);
     formData.append("password", e.target[2].value);
     formData.append("phone", e.target[3].value);
     formData.append("image", e.target[4].files[0]);
     try {
-        // const user = { name: e.target[0].value };
+        // const URL = "http://localhost:3000/api/v1/appadel/users";
+        // const user = { name: e.target[0].value, email: e.target[1].value, password: e.target[2].value, phone: e.target[3].value };
         // , email: e.target[1].value, password: e.target[2].value, phone: e.target[3].value
         // console.log("User Data =>", user);
         // const userJSON = JSON.stringify(user);
-        // console.log("JSON.stringify(user) =>", userJSON);
 
-        const response = await fetch(URL + "/register", {
-            // body: userJSON,
+        const response = await fetch("http://localhost:3000/api/v1/appadel/users/register", {
+            // headers: { "Content-Type": "application/json", },
             method: "POST",
-            body: formData
-            // headers: { "Content-type": "multipart form data" }
+            // body: userJSON,
+            body: formData,
         });
-        console.log("response =>", response);
+        console.log("response fetch =>", response);
 
         const res = await response.json();
-        e.preventDefault();
+        // e.preventDefault();
         console.log("res FINAL =>", res);
         console.log("Usuario registrado correctamente.");
 
