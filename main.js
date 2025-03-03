b44c3a2b9dd4d6000ed4d7dd1d3c4a80dadee3df
Daniel Salvatierra <dss250583@gmail.com>
2025-03-03 16:18:11 +0100

    FIX Main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+import "./style.css";
 import { Header } from "./src/components/Header/Header";
 import { Main } from "./src/components/Main/Main";
 import { Home } from "./src/pages/Home/Home";
-import "./style.css";
 
 // localStorage.clear();
 Header();
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import "./main.css";
+import "./Main.css";
 
 export const Main = () => {
     const main = document.createElement("main");
